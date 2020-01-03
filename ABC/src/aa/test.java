9302b202698594070377ba9f606adf3a8c4a90d6
000 <000@192.168.21.45>
2020-01-03 11:54:22 +0800

    4

--- ABC/src/aa/test.java
+++ ABC/src/aa/test.java
@@ -4,5 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		int a = 5;	
 		System.out.println(a);
+		System.out.println("/*//*/*/*/*/*/*/*/*/*/*");
+		System.out.println("/*//*/*/*/*/*/*/*/*/*/*");
 	}
 }
